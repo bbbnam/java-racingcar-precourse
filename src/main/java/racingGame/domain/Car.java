dac2b589da0b6c4d9b582340740d4b6dae6160f2
bbbnam <bbbnam@naver.com>
2021-05-03 08:54:46 +0900

    feat : 자동차 객체 생성 구현

---
+++ src/main/java/racingGame/domain/Car.java
@@ -0,0 +1,28 @@
+package racingGame.domain;
+
+import java.util.Objects;
+
+public class Car {
+
+    private final String name;
+    private final int position;
+
+    public Car(String name, int position) {
+        this.name = name;
+        this.position = position;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Car car = (Car) o;
+        return position == car.position &&
+                Objects.equals(name, car.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, position);
+    }
+}
