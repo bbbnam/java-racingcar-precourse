86109031553f9f3a74ac0b7f04b644a30f01d89f
bbbnam <bbbnam@naver.com>
2021-05-03 14:19:46 +0900

    test : 자동차 이름 예외 요구사항 테스트

--- src/test/java/racingGame/domain/CarTest.java
+++ src/test/java/racingGame/domain/CarTest.java
@@ -1,13 +1,22 @@
 package racingGame.domain;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class CarTest {
 
-    private final  Car car = new Car("자동차1", 0);
+    private Car car;
+
+    @BeforeEach
+    void setUp() {
+        car = new Car("자동차1", 0);
+    }
 
     @DisplayName("자동차 객체 생성 - 이름과 이동상태를 가지고 있음")
     @Test
@@ -25,5 +34,14 @@ class CarTest {
     @Test
     void stop() {
         assertThat(car.move(() -> false)).isEqualTo(new Car("자동차1", 0));
+    }
+
+    @DisplayName("자동차 이름은 5자 이하인지 테스트 - 5자 초과시 예외발생 테스트")
+    @ParameterizedTest
+    @ValueSource(strings = {"crongs", "honux2", "pororo", "loopy&patty"})
+    void validateCarName(String carName) {
+        assertThatThrownBy(() -> new Car(carName))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("이름은 5자 이하만 가능합니다.");
     }
 }
