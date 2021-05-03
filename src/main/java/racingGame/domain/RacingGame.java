c2ecec3fc11c5ce8001ecfac7e545dec3b30026b
bbbnam <bbbnam@naver.com>
2021-05-03 22:47:14 +0900

    feat : 유효성 검사 기능 구현(tryCount - 0 또는 음수불가)

--- src/main/java/racingGame/domain/RacingGame.java
+++ src/main/java/racingGame/domain/RacingGame.java
@@ -4,13 +4,22 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class RacingGame {
+    private static final int TRY_COUNT_MIN = 1;
+    private static final String TRY_COUNT_MIN_MESSAGE = "시도 횟수는 적어도 1보단 커야 합니다.";
 
     private final Cars cars;
     private final int tryCount;
 
     public RacingGame(List<String> carNames, int tryCount) {
+        validateTryCount(tryCount);
         this.cars = initCars(carNames);
         this.tryCount = tryCount;
+    }
+
+    private void validateTryCount(int tryCount) {
+        if (tryCount < TRY_COUNT_MIN) {
+            throw new IllegalArgumentException(TRY_COUNT_MIN_MESSAGE);
+        }
     }
 
     private Cars initCars(List<String> carNames) {
