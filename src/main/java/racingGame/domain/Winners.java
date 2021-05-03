5392b8339bf5d81d908ebff1447c6df31fc0a5d1
bbbnam <bbbnam@naver.com>
2021-05-03 10:28:11 +0900

    feat : 우승자 찾기 기능 구현 관련 빈 객체 생성

---
+++ src/main/java/racingGame/domain/Winners.java
@@ -0,0 +1,16 @@
+package racingGame.domain;
+
+import java.util.List;
+
+public class Winners {
+
+    private final Cars records;
+
+    public Winners(Cars records) {
+        this.records = records;
+    }
+
+    public List<Car> findWinner() {
+
+    }
+}
